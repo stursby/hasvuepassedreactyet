920877bd4df3489abd847fe14123eefd0c835dcb
Charlie Hield <charlie.hield@collemcvoy.com>
2018-06-13 15:05:59 -0500

    Use vanilla GA instead

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
-
-Vue.use(VueAnalytics, {
-  id: 'UA-120813208-1'
-})
 
 new Vue({
   render: h => h(App)
